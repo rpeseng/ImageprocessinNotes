8fc8b84c76f9fc6c8607ad05c36b5d2311b9c3ca
rpeseng <rojhan2147@gmail.com>
2022-05-07 19:02:22 -0400

    My first github testing

--- BasicImageOperations/ReadAndWriteImage.py
+++ BasicImageOperations/ReadAndWriteImage.py
@@ -1,3 +1,12 @@
 import cv2
 
-print("Deneme")
+#An image reading and showing
+image = cv2.imread('nature.jpeg',0)
+cv2.imshow('NatureImage',image)
+
+#An image writing
+cv2.imwrite('gri.png',image)
+
+
+cv2.waitKey(0)
+cv2.destroyWindow()
